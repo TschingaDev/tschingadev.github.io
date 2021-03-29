9890d025c23b35db3f322c8cabebba8cdc59b762
MaxHuerlimann <max.huerlimann@gmail.com>
2021-03-29 17:22:54 +0200

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,13 @@ cd dist
 # if you are deploying to a custom domain
 echo 'www.tschinga.ch' > CNAME
 
+# Disable jekyll
+touch .nojekyll
+
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:MaxHuerlimann/tschinga.git master:gh-pages
+git push -f git@github.com:TschingaDev/tschingadev.github.io.git master:gh-pages
 
 cd -

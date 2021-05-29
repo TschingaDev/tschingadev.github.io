e2b7e8bd2979c202c80df11b581e17f73441a3a7
MaxHuerlimann <max.huerlimann@gmail.com>
2021-05-29 23:53:22 +0200

    Revert to vue 2

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,5 @@
 module.exports = {
   chainWebpack: (config) => {
-    config.resolve.alias.set('vue', '@vue/compat');
-
-    config.module
-      .rule('vue')
-        .test(/\.vue$/)
-        .use('vue-loader')
-        .tap((options) => ({
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2,
-            },
-          },
-        }));
-
     config.module
       .rule('raw')
         .test(/\.txt$/)

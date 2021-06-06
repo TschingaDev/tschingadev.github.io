82eb9b44d5c8805e490a11fb62e53ac3bf951d1e
MaxHuerlimann <max.huerlimann@gmail.com>
2021-06-06 22:04:24 +0200

    Add contact information

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,19 @@
 <template lang="html">
-  <b-container class="contact">
-    contact Component
+  <b-container>
+    <b-container fluid class='image-container'>
+      <img src='@/assets/tschinga_mail.png' class='image'/>
+    </b-container>
+    <b-container class="contact">
+      <p>Breitensteinstrasse 19A</p>
+      <p>8037 Zürich</p>
+    </b-container>
+    <b-container fluid class='image-container'>
+      <img src='@/assets/tschinga_tag.png' class='image'/>
+    </b-container>
   </b-container>
 </template>
 
-<script lang="js">
-
+<script>
 export default {
   name: 'contact',
   props: [],
@@ -17,18 +25,23 @@ export default {
 
     };
   },
-  methods: {
-
-  },
-  computed: {
-
-  },
 };
 
 </script>
 
 <style scoped lang="scss">
-  .contact {
-
-  }
+p {
+  margin: 0;
+  padding: 0;
+}
+.contact {
+  text-align: left;
+}
+.image-container {
+  width: 80vw;
+  padding: 0;
+}
+.image {
+  width: 80%;
+}
 </style>

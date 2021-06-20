d54d505ebdc02e4cccbb359d424a0080002bcbeb
MaxHuerlimann <max.huerlimann@gmail.com>
2021-06-20 18:22:05 +0200

    Add staging deployment script

---
+++ deploy_staging.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# Disable jekyll
+touch .nojekyll
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:TschingaDev/tschingaTest.git staging:gh-pages
+
+cd -

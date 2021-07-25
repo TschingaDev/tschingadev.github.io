d8a379a9562ad026252914c4c0c8a5c18e059a2e
MaxHuerlimann <max.huerlimann@gmail.com>
2021-07-25 22:18:28 +0200

    Use correct staging url

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:TschingaDev/tschingaTest.git staging:gh-pages
+git push -f git@github.com:MaxHuerlimann/tschingaStaging.git master:gh-pages
 
 cd -
